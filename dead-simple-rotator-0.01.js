52fbce581873d13504aad1764e5ed034b960e125
Gideon Barton <k4t434sispho3nix@gmail.com>
2011-10-12 15:24:35 -0400

    Turned function into jQuery plugin.

--- dead-simple-rotator-0.01.js
+++ dead-simple-rotator-0.01.js
@@ -21,33 +21,36 @@
 *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-function rotator(div, easing, easingLast, duration, durationLast, intervalDuration, imgWidth)
-{
-	var frameWidth = imgWidth / 4;
-	var margins    = [(frameWidth * 1), (frameWidth * 2)];
+(function($) {
+	$.fn.rotator = function(easing, easingLast, duration, durationLast, intervalDuration, imgWidth)
+	{
+		var div = this.attr('id') + ' img';
+		var frameWidth = imgWidth / 4;
+		var margins    = [(frameWidth * 1), (frameWidth * 2)];
 
-	var daemon = setInterval(
-		function() {
-			var offset = $('#' + div).css('margin-left');
-			
-			switch (offset)
-			{
-				case '0px':
-					$('#' + div).animate({					    
-					    'margin-left': '-' + margins[0] + 'px',
-					  }, duration, easing);
-					break;
-				case '-854px':
-					$('#' + div).animate({					    
-					    'margin-left': '-' + margins[1] + 'px',
-					  }, duration, easing);
-					break;
-				case '-1708px':
-					$('#' + div).animate({					    
-					    'margin-left': '0px',
-					  }, durationLast, easingLast);
-					break;
-			}
-		}, intervalDuration
-	)
-}
+		var daemon = setInterval(
+			function() {
+				var offset = $('#' + div).css('margin-left');
+				
+				switch (offset)
+				{
+					case '0px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[0] + 'px',
+						  }, duration, easing);
+						break;
+					case '-854px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[1] + 'px',
+						  }, duration, easing);
+						break;
+					case '-1708px':
+						$('#' + div).animate({					    
+						    'margin-left': '0px',
+						  }, durationLast, easingLast);
+						break;
+				}
+			}, intervalDuration
+		);
+	};
+}) (jQuery);
