9b600e01e98e88b029b26ff00adb4e0a4206ea6d
Gideon Barton <k4t434sispho3nix@gmail.com>
2011-10-14 14:13:54 -0400

    V0.02 first commit. Added button functionality and included sample css and associated button images and source .psd file.

---
+++ dead-simple-rotator.0.02.js
@@ -0,0 +1,124 @@
+/*
+*	Dead Simple Rotator v0.01
+*	
+*	Author: Gideon Barton 2011 gideon.barton@gmail.com
+*	Requirements: jQuery, jQueryUI
+*
+*	Copyright (C) 2011  Gideon Barton
+*	
+*	This program is free software; you can redistribute it and/or
+*	modify it under the terms of the GNU General Public License
+*	as published by the Free Software Foundation; either version 2
+*	of the License, or (at your option) any later version.
+*	
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*	
+*	You should have received a copy of the GNU General Public License
+*	along with this program; if not, write to the Free Software
+*	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+(function($) {
+	$.fn.rotator = function(easing, easingLast, duration, durationLast, intervalDuration, imgWidth)
+	{
+		var div = this.attr('id') + ' img';
+		var frameWidth = imgWidth / 4;
+		var margins    = [(frameWidth * 1), (frameWidth * 2), (frameWidth * 3)];
+
+		$('#rotator-prev, #rotator-next').click(function()
+		{
+			var direction;
+
+			$(this).attr('id') === 'rotator-prev'
+			? direction = 'prev'
+			: direction = 'next';
+
+			switch (direction)
+			{
+				case 'prev':
+					switch ($('#' + div).css('margin-left'))
+					{
+						case '0px':
+							$('#' + div).animate({					    
+							    'margin-left': '-' + margins[2] + 'px',
+							  }, (duration / 2), easing);
+							break;
+						case '-854px':
+							$('#' + div).animate({					    
+							    'margin-left': '0px',
+							  }, (duration / 2), easingLast);
+							break;
+						case '-1708px':
+							$('#' + div).animate({					    
+							    'margin-left': '-' + margins[0] + 'px',
+							  }, (duration / 2), easing);
+							break;
+						case '-2562px':
+							$('#' + div).animate({					    
+							    'margin-left': '-' + margins[1] + 'px',
+							  }, (duration / 2), easing);
+						break;
+					}
+					break;
+				case 'next':
+					switch ($('#' + div).css('margin-left'))
+					{
+						case '0px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[0] + 'px',
+						  }, (duration / 2), easing);
+						break;
+					case '-854px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[1] + 'px',
+						  }, (duration / 2), easing);
+						break;
+					case '-1708px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[2] + 'px',
+						  }, (duration / 2), easingLast);
+						break;
+					case '-2562px':
+						$('#' + div).animate({					    
+						    'margin-left': '0px',
+						  }, (duration / 2), easingLast);
+						break;
+					}
+					break;
+			}
+		});
+
+		var daemon = setInterval(
+			function() {
+				var offset = $('#' + div).css('margin-left');
+				
+				switch (offset)
+				{
+					case '0px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[0] + 'px',
+						  }, duration, easing);
+						break;
+					case '-854px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[1] + 'px',
+						  }, duration, easing);
+						break;
+					case '-1708px':
+						$('#' + div).animate({					    
+						    'margin-left': '-' + margins[2] + 'px',
+						  }, durationLast, easingLast);
+						break;
+					case '-2562px':
+						$('#' + div).animate({					    
+						    'margin-left': '0px',
+						  }, durationLast, easingLast);
+						break;
+				}
+			}, intervalDuration
+		);
+	};
+}) (jQuery);
